29bf14b5dd265ea0c04260c393f4b5f161fa4271
madvil2 <madvil2@mail.ru>
2018-08-26 23:13:39 +0300

    rush00

---
+++ rush00/ex00/rush04.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush04.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pcollio- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/26 19:45:02 by pcollio-          #+#    #+#             */
+/*   Updated: 2018/08/26 23:04:45 by pcollio-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#define LEFT_TOP_CHAR			'A'
+#define RIGHT_TOP_CHAR			'C'
+#define LEFT_BOT_CHAR			'C'
+#define RIGHT_BOT_CHAR			'A'
+#define TOP_BOT_SIDE_CHAR		'B'
+#define LEFT_RIGHT_SIDE_CHAR	'B'
+
+int		ft_putchar(char c);
+
+void	create(int x, int y, int a, int b)
+{
+	if (a == 1 && b == 1)
+		ft_putchar(LEFT_TOP_CHAR);
+	else if (a == 1 && b == x)
+		ft_putchar(RIGHT_TOP_CHAR);
+	else if (a == y && b == 1)
+		ft_putchar(LEFT_BOT_CHAR);
+	else if (a == y && b == x)
+		ft_putchar(RIGHT_BOT_CHAR);
+	else if (a == 1 || a == y)
+		ft_putchar(TOP_BOT_SIDE_CHAR);
+	else if (b == 1 || b == x)
+		ft_putchar(LEFT_RIGHT_SIDE_CHAR);
+	else
+		ft_putchar(' ');
+}
+
+void	rush(int x, int y)
+{
+	int a;
+	int b;
+
+	a = 0;
+	if (x <= 0 || y <= 0)
+		return ;
+	else
+	{
+		while (++a <= y)
+		{
+			b = 0;
+			while (++b <= x)
+			{
+				create(x, y, a, b);
+			}
+			ft_putchar('\n');
+		}
+	}
+}
